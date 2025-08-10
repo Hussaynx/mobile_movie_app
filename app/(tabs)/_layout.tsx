74c796c52aa9f2747ba7a33c49bef1cd69fb4a9c
Hussaynx <M.hussainshakeel@gmail.com>
2025-08-10 16:19:05 +0300

    Adding components to the home page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -26,6 +26,24 @@ const _layout = () => {
     <Tabs
     screenOptions={{
       tabBarShowLabel: false,
+      tabBarItemStyle: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+      },
+      tabBarStyle: {
+        backgroundColor: '#0f0D23',
+        borderRadius: 50,
+        marginHorizontal: 20,
+        marginBottom: 36,
+        height: 52,
+        position: 'absolute',
+        overflow: 'hidden',
+        borderWidth: 1,
+        borderColor: '#0f0D23',
+      }
     }}>
         <Tabs.Screen
           name="index"
